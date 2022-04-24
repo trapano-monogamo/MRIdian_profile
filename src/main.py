8182d164c9b04e9ca7b25d03bed582ab3232b3c4
trapano.monogamo <filippo.cozzi.fc@gmail.com>
2022-04-24 17:20:15 +0200

    made tests with moving average

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from MyCacher import *
 
 def main():
-   # c = Cacher("./res/", "./output tests/original_not_filtered/")
-   # c = Cacher("./res/", "./output tests/dose_filtered/")
-   # c = Cacher("./res/", "./output tests/first_derivative_filtered/")
-   # c = Cacher("./res/", "./output tests/second_derivative_filtered/")
-   # c = Cacher("./res/", "./output tests/both_derivatives_filtered/")
-   c = Cacher("./res/", "./output tests/all_filtered/")
+   # c = Cacher("./res/", "./output tests/original_not_filtered/", TestSettings(moving_average, 3, 0))
+   # c = Cacher("./res/", "./output tests/dose_filtered/", TestSettings(moving_average, 3, 1))
+   # c = Cacher("./res/", "./output tests/first_derivative_filtered/", TestSettings(moving_average, 3, 2))
+   # c = Cacher("./res/", "./output tests/second_derivative_filtered/", TestSettings(moving_average, 3, 3))
+   # c = Cacher("./res/", "./output tests/both_derivatives_filtered/", TestSettings(moving_average, 3, 4))
+   c = Cacher("./res/", "./output tests/all_filtered/", TestSettings(moving_average, 3, 5))
 
 if __name__ == '__main__':
    main()
