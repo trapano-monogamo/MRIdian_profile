d9daed551cc2b438e667fe7220b65450b1db49ba
trapano.monogamo <filippo.cozzi.fc@gmail.com>
2022-04-25 16:40:31 +0200

    TeSt

--- src/main.py
+++ src/main.py
@@ -4,10 +4,12 @@ def main():
    # c = Cacher("./res/", "./output tests/original_not_filtered/", TestSettings(0, moving_average, [3]))
    # c = Cacher("./res/", "./output tests/dose_filtered - spline/", TestSettings(1, "spline"))
    # c = Cacher("./res/", "./output tests/first_derivative_filtered/", TestSettings(2, moving_average))
-   c = Cacher("./res/", "./output tests/dose_first_derivative_filtered/", TestSettings(3, "moving_average"))
+   # c = Cacher("./res/", "./output tests/dose_first_derivative_filtered/", TestSettings(3, "moving_average"))
    # c = Cacher("./res/", "./output tests/second_derivative_filtered/", TestSettings(4, moving_average, [3]))
    # c = Cacher("./res/", "./output tests/both_derivatives_filtered/", TestSettings(5, moving_average, [3]))
    # c = Cacher("./res/", "./output tests/all_filtered/", TestSettings(6, moving_average, [3]))
+
+   c = Cacher("./res/", "./with sanpietro 20x20 0.1mm - moving_average - dose_first_derivative_filtered/", TestSettings(3, "moving_average"))
 
 if __name__ == '__main__':
    main()
