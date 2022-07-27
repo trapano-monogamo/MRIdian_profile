db53cafba46b206c0e6ab9cb5e9d786d9ade7fee
trapano.monogamo <filippo.cozzi.fc@gmail.com>
2022-07-27 17:05:30 +0200

    tested new parameters on both MRI and TRUEBEAM

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from MyCacher import *
 
 def main():
-   # Cacher("./res/MRI/", f"./out/MRI/no MA - fit 0.001mm/", 0.001)
-   Cacher("./res/TRUEBEAM/",
-              "./out/TRUEBEAM/no MA - fit 0.1mm",
-              0.1)
+   Cacher(
+      "./res/MRI/",
+      f"./out/MRI/test 0.1mm/",
+      0.1)
+   Cacher(
+      "./res/TRUEBEAM/",
+      "./out/TRUEBEAM/test 0.1mm",
+      0.1)
 
 if __name__ == '__main__':
    main()
