7ef84b5c3b2bc8f0f046054d7cc230d8247450cb
trapano.monogamo <filippo.cozzi.fc@gmail.com>
2022-07-29 11:16:23 +0200

    single threaded teset at 0.1mm and 0.01mm, both datasets

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 from MyCacher import *
+import time
 
 def main():
+   start_time = time.time()
+
    Cacher(
       "./res/MR_LINAC/",
-      f"./out/MR_LINAC/results 0.001mm/",
-      0.001)
+      f"./out/MR_LINAC/singlethreaded test 0.01mm/",
+      0.01)
    Cacher(
       "./res/TRUEBEAM/",
-      "./out/TRUEBEAM/results 0.001mm",
-      0.001)
+      "./out/TRUEBEAM/singlethreaded test 0.01mm",
+      0.01)
+
+   end_time = time.time()
+   print(f"exec time: {end_time - start_time}")
 
 if __name__ == '__main__':
    main()
