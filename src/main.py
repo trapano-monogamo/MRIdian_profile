48f0244caeb8c164c41720054b54191250397343
trapano.monogamo <filippo.cozzi.fc@gmail.com>
2022-09-13 13:58:41 +0200

    re-tabbed. tab-only, 4 spaces. that's the way

--- src/main.py
+++ src/main.py
@@ -4,27 +4,31 @@ import time
 
 # hello
 
+
 def main():
-   start_time = time.time()
+    start_time = time.time()
 
-   # p1 = multiprocessing.Process(target = Cacher, args = ("./res/MR_LINAC/", f"./out/MR_LINAC/multithreaded test 0.1mm/", 0.1))
-   # p1.start()
-   # p2 = multiprocessing.Process(target = Cacher, args = ("./res/TRUEBEAM/", "./out/TRUEBEAM/multithreaded test 0.1mm", 0.1))
-   # p2.start()
+    # p1 = multiprocessing.Process(target = Cacher, args = ("./res/MR_LINAC/", f"./out/MR_LINAC/multithreaded test 0.1mm/", 0.1))
+    # p1.start()
+    # p2 = multiprocessing.Process(target = Cacher, args = ("./res/TRUEBEAM/", "./out/TRUEBEAM/multithreaded test 0.1mm", 0.1))
+    # p2.start()
 
-   # p1.join()
-   # p2.join()
+    # p1.join()
+    # p2.join()
 
-   p1 = multiprocessing.Process(target = Cacher, args = ("./res/TEST_PROFILE/", "./out/TEST_PROFILE 0.1mm/", 0.1))
-   p1.start()
-   p2 = multiprocessing.Process(target = Cacher, args = ("./res/TEST_PROFILE/", "./out/TEST_PROFILE 0.001mm/", 0.001))
-   p2.start()
+    p1 = multiprocessing.Process(target=Cacher, args=(
+        "./res/TEST_PROFILE/", "./out/temp/", 0.1))
+    p1.start()
+    p2 = multiprocessing.Process(target=Cacher, args=(
+        "./res/TEST_PROFILE/", "./out/temp/", 0.1))
+    p2.start()
 
-   p1.join()
-   p2.join()
+    p1.join()
+    p2.join()
 
-   end_time = time.time()
-   print(f"exec time: {end_time - start_time}")
+    end_time = time.time()
+    print(f"exec time: {end_time - start_time}")
+
 
 if __name__ == '__main__':
-   main()
+    main()
